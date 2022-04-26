14e52142296be03a696b791a2e00ebdddd417412
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2022-04-26 14:40:58 +0000

    Finishing the A and B matrix

--- MatrixCalc.py
+++ MatrixCalc.py
@@ -1,12 +1,12 @@
 n = int(input("Enter the Order Of matrix 'n' : "))
-
+A = []
 for i in range(1, n+1):
-
+    exec("%s = []" % ("A"+str(i)))
     for j in range(1, n+1):
         Dynamic_Variable_Name = "A"+str(i)+str(j)
         ElementValue = int(input("A"+str(i)+str(j)+" : "))
-        exec("%s = %d" % (Dynamic_Variable_Name, ElementValue))
-
+        exec("%s.append(%d)" % ("A"+str(i), ElementValue))
+    exec("A.append(%s)" % ("A"+str(i)))
 
 if (n % 2) == 0:
     B = []
@@ -18,4 +18,5 @@ else:
     for BPairValue in range(1, round(n/2)):
         B.append(BPairValue)
         B.append(-BPairValue)
+print(A)
 print(B)

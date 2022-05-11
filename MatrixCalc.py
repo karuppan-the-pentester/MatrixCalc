2349111f68f4323da3e0c200d5104d838a1188e3
Karuppasamy K <75080347+karuppan-the-pentester@users.noreply.github.com>
2022-05-11 19:10:06 +0000

    Update MatrixCalc.py

--- MatrixCalc.py
+++ MatrixCalc.py
@@ -1,8 +1,10 @@
 import collections
 from itertools import permutations
 import numpy as np
+
 n = int(input("Enter the Order Of matrix 'n' : "))
 A = []
+
 for i in range(1, n+1):
     exec("%s = []" % ("A"+str(i)))
     for j in range(1, n+1):
@@ -10,6 +12,7 @@ for i in range(1, n+1):
         ElementValue = int(input("A"+str(i)+str(j)+" : "))
         exec("%s.append(%d)" % ("A"+str(i), ElementValue))
     exec("A.append(%s)" % ("A"+str(i)))
+
 if (n % 2) == 0:
     BTemp = []
     for BTempPairValue in range(1, round(n/2+1)):
@@ -41,14 +44,22 @@ for i in range(0, n):
         exec("%s.append(0)" % ("R"+str(i)))
     exec("result.append(%s)" % ("R"+str(i)))
 
+MatrixResultChecker = 0
+
 for B in li:
     result = np.dot(A, list(B))
     ZeroChecker = [item for item,
                    count in collections.Counter(result).items() if count > 1]
+    if sum(result) == 0 and 0 in result and len(ZeroChecker) == 0:
+        print("A x "+str(B)+" => "+str(result))
+        MatrixResultChecker = 1
 
-    if (n % 2) == 0:
-        if sum(result) == 0 and 0 not in result:
-            print("A x "+str(B)+" => "+str(result))
-    else:
-        if sum(result) == 0 and 0 in result and 0 not in ZeroChecker:
-            print("A x "+str(B)+" => "+str(result))
+for B in li:
+    result = np.dot(A, list(B))
+    ZeroChecker = [item for item,
+                   count in collections.Counter(result).items() if count > 1]
+    if sum(result) == 0 and 0 in result and len(ZeroChecker) == 0:
+        print(str(result))
+
+if MatrixResultChecker == 0:
+    print("There is no-more value")
